04625e89f11dde68fad2bd8bd35fd9c90e099765
rok <roky.zz@gmail.com>
2018-10-11 17:58:38 +0200

    check units and numbers code before converting

--- routes/api.js
+++ routes/api.js
@@ -20,17 +20,20 @@ module.exports = function (app) {
 		var input = req.query.input;
 		var initNum = convertHandler.getNum(input);
 		var initUnit = convertHandler.getUnit(input);
+
+		if (!initNum && !initUnit) {
+			return res.send({ error: "invalid number and unit" })
+		} else if (!initNum) {
+			return res.send({ error: "invalid number" })
+		} else if (!initUnit) {
+			return res.send({ error: "invalid unit" })
+		}
+
 		var returnNum = convertHandler.convert(initNum, initUnit);
 		var returnUnit = convertHandler.getReturnUnit(initUnit);
 		var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-		if (!initNum && !initUnit) {
-			return res.send("invalid number and unit")
-		} else if (!initNum) {
-			return res.send("invalid number")
-		} else if (!initUnit) {
-			return res.send("invalid unit")
-		}
+
 		res.json({
 			initNum: parseResultInteger(initNum),
 			initUnit: ["l", "L"].indexOf(initUnit) > -1 ? "L" : initUnit.toLowerCase(),
