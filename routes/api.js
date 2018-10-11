4a6c321ab5602b6f7a35239f0c28a9ffbfacf356
rok <roky.zz@gmail.com>
2018-10-11 16:13:28 +0200

    complete api route return json

--- routes/api.js
+++ routes/api.js
@@ -12,19 +12,35 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
     .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+		var input = req.query.input;
+		var initNum = convertHandler.getNum(input);
+		var initUnit = convertHandler.getUnit(input);
+		var returnNum = convertHandler.convert(initNum, initUnit);
+		var returnUnit = convertHandler.getReturnUnit(initUnit);
+		var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+		if (!initNum && !initUnit) {
+			return res.send("invalid number and unit")
+		} else if (!initNum) {
+			return res.send("invalid number")
+		} else if (!initUnit) {
+			return res.send("invalid unit")
+		}
+		res.json({
+			initNum: parseResultInteger(initNum),
+			initUnit,
+			returnNum: parseResultInteger(returnNum),
+			returnUnit,
+			string: toString
+		});
     });
-    
 };
+
+function parseResultInteger(input) {
+	return Number.isInteger(+input) ? input : Math.round(input * 100000) / 100000;
+}
